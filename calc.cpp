ca880a8abd9b5bb02fa1c3eba3fd882db280e8db
Jason King <kingj@bhsconnect.org>
2016-09-30 16:51:17 -0400

    Added implementation of division function

--- calc.cpp
+++ calc.cpp
@@ -15,8 +15,8 @@ int multiply(int num1, int num2) {
 	return num1 * num2;
 }
 
-//need to implement
 int divide(int num1, int num2) {
+	return num1 / num2;
 }
 
 
@@ -28,6 +28,7 @@ int num2 = 30;
 cout << num1 << " + " << num2 << " = " << add(num1, num2) << endl;
 cout << num1 << " - " << num2 << " = " << subtract(num1, num2) << endl;
 cout << num1 << " * " << num2 << " = " << multiply(num1, num2) << endl;
+cout << num1 << " / " << num2 << " = " << divide(num1, num2) << endl;
 
 return 0;
 }
